12b7d4ce0dfa4bf0b4cdfd2bafedd6f5c595a2f9
Nivedha-85 <60845904+Nivedha-85@users.noreply.github.com>
2020-09-29 23:39:20 +0530

    Add files via upload
    working CRC

--- CodeRedundancyCheck.java
+++ CodeRedundancyCheck.java
@@ -5,21 +5,23 @@ public class CodeRedundancyCheck {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int dividend[] = new int[19];
-		int divisor[] = new int[] {1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1};
-		int remainder[] = new int[17];
+		int divisor[] = new int[] {1,1,0,0,0,0,0,0,0,0,0,1,0,0,0,1};
+		int remainder[] = new int[16];
 		int dataword[] = new int[19];
 		int codeword[] = new int[19];
-		int r_s[] = new int[17];
+		int r_s[] = new int[16];
 		int n = 0;
 		//System.out.println(dividend.length);
 		System.out.println("Enter the size of the dividend");
 		n = sc.nextInt();
+		int[] originalCodeWord = new int[n];
 		System.out.println("Enter the dividend");
 		for(int i=0;i<n;i++)
 		{
 			dividend[i] = sc.nextInt();
 			dataword[i] = dividend[i];
 			codeword[i] = dividend[i];
+			originalCodeWord[i] = dividend[i];
 		}
 		
 		/*System.out.println("Enter the divisor");
@@ -37,7 +39,7 @@ public class CodeRedundancyCheck {
 		}
 		
 		System.out.println("The dataword is:");
-		for(int h=0;h<dataword.length;h++)
+		for(int h=0;h<dataword.length-1;h++)
 		{
 			System.out.print(dataword[h] + " ");
 		}
@@ -46,44 +48,15 @@ public class CodeRedundancyCheck {
 		remainder = division(dataword,divisor);
 		
 		System.out.println("The remainder at sender site");
-		for(int y=0;y<remainder.length;y++)
+		for(int y=0;y<remainder.length-1;y++)
 		{
 			System.out.print(remainder[y]+ " ");
 		}
 		System.out.println();
-		int re = 2;
-		for(int r=0;r<remainder.length;r++) {
-			codeword[re+r] = remainder[r];
-			//re++;
-		}
 		
-		System.out.println("The codeword is:");
-		for(int u=0;u<codeword.length;u++)
-		{
-			System.out.print(codeword[u] + " ");
-		}
-		System.out.println();
+		receiver(originalCodeWord,remainder,divisor);
 		
-		r_s = division(codeword,divisor);
-		System.out.println("The receiver remainder is:");
-		for(int f=0;f<r_s.length;f++)
-		{
-			System.out.print(r_s[f] + " ");
-		}
-		System.out.println();
-		int sum =0;
-		for(int f=0;f<r_s.length;f++)
-		{
-			sum = sum + r_s[f];
-		}
-		if(sum == 0)
-		{
-			System.out.println("No syndrome,extract the dataword");
-		}
-		else
-		{
-			System.out.println("Syndrome,don't extract the dataword");
-		}
+		
 		sc.close();
 	}
 	public static int[] division(int []dividend,int[]divisor) {
@@ -101,7 +74,7 @@ public class CodeRedundancyCheck {
 			array[n] = 0;
 		}
 		// Based on divisor length
-		for(int c=0;c<16;c++)
+		for(int c=0;c<3;c++)
 		{
 			
 			if(r1[0] == 1)
@@ -137,15 +110,61 @@ public class CodeRedundancyCheck {
 					
 			}				
 		}
-		for(int u=0;u<remainder.length;u++)
+		for(int u=0;u<remainder.length-1;u++)
 		{
 			remainder[u] = r1[u];
 		}
 		return remainder;
 	}
+	public static void receiver(int []originalCodeWord,int[] remainder,int [] divisor)
+	{
+		System.out.println(originalCodeWord.length + " " + remainder.length);
+		int [] sendercodeword = new int[originalCodeWord.length + remainder.length];
+		int initPoint = originalCodeWord.length;
+		int i=0;
+		for(i=0;i<originalCodeWord.length;i++)
+		{
+			sendercodeword[i] = originalCodeWord[i];
+		}
+		for(int j=0;j<remainder.length;j++)
+		{
+			sendercodeword[i] = remainder[j];
+			i++;
+			
+		}
+		
+		//System.out.println(sendercodeword);
+		System.out.println("The codeword is:");
+		for(int u=0;u<sendercodeword.length;u++)
+		{
+			System.out.print(sendercodeword[u] + " ");
+		}
+		System.out.println();
+		
+		int r[] = new int[17];
+		r = division(sendercodeword,divisor);
+		
+		System.out.println("The receiver remainder is:");
+		for(int f=0;f<r.length;f++)
+		{
+			System.out.print(r[f] + " ");
+		}
+		System.out.println();
+		int sum =0;
+		for(int f=0;f<r.length;f++)
+		{
+			sum = sum + r[f];
+		}
+		if(sum == 0)
+		{
+			System.out.println("No syndrome,extract the dataword");
+		}
+		else
+		{
+			System.out.println("Syndrome,don't extract the dataword");
+		}
+	}
+
 }
 
 
-
-
-	
